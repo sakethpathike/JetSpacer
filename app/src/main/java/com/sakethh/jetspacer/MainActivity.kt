4855960d376a2376bb81d91f9ecd992bc2de6a47
Saketh <83284398+sakethpathike@users.noreply.github.com>
2024-10-25 01:52:19 +0530

    Add NavHost for navigation

--- app/src/main/java/com/sakethh/jetspacer/MainActivity.kt
+++ app/src/main/java/com/sakethh/jetspacer/MainActivity.kt
@@ -4,17 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.sakethh.jetspacer.news.presentation.NewsScreen
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.sakethh.jetspacer.news.presentation.TopHeadlineDetailScreen
+import com.sakethh.jetspacer.news.presentation.TopHeadlinesScreen
 import com.sakethh.jetspacer.ui.theme.JetSpacerTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             JetSpacerTheme {
-                NewsScreen()
+                Surface {
+                    NavHost(
+                        navController = navController,
+                        startDestination = TopHeadlinesScreen
+                    ) {
+                        composable<TopHeadlinesScreen> {
+                            TopHeadlinesScreen(navController)
+                        }
+                        composable<TopHeadlineDetailScreen> { navBackStackEntry ->
+                            TopHeadlineDetailScreen(
+                                navBackStackEntry.toRoute<TopHeadlineDetailScreen>().rawArticle
+                            )
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+
+@Serializable
+object TopHeadlinesScreen
+
+@Serializable
+data class TopHeadlineDetailScreen(
+    val rawArticle: String
+)
+
