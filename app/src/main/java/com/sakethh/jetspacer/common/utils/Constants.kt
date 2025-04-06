8ad7320f1279519217345f39540fd0c561683dcd
sakethpathike <sakethpathike@gmail.com>
2025-04-06 17:58:22 +0530

    comment: API keys are public, i know

--- app/src/main/java/com/sakethh/jetspacer/common/utils/Constants.kt
+++ app/src/main/java/com/sakethh/jetspacer/common/utils/Constants.kt
@@ -1,7 +1,10 @@
 package com.sakethh.jetspacer.common.utils
 
 object Constants {
+
+    // usually API keys aren't supposed to be public for good reasons, but in this case idc
     const val NASA_API_KEY = "bCUlmKS9EpyQ1ChMIUnsglSTGTwQy1CdLhLP4FCL"
     const val NEWS_API_KEY = "27d2f9412f1148e6ba7b544336844ced"
+
     const val DATA_STORE_NAME = "jetspacer datastore"
 }
