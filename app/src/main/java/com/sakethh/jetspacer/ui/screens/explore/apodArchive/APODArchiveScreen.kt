72a2f6aa3dd150d4cd5e627f9342d8b72f6cb8fa
sakethpathike <sakethpathike@gmail.com>
2025-08-15 20:32:36 +0530

    feat: add border to APOD images in `APODArchiveScreen`

--- app/src/main/java/com/sakethh/jetspacer/ui/screens/explore/apodArchive/APODArchiveScreen.kt
+++ app/src/main/java/com/sakethh/jetspacer/ui/screens/explore/apodArchive/APODArchiveScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -38,6 +40,7 @@ import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
@@ -50,6 +53,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -171,17 +175,25 @@ fun SharedTransitionScope.APODArchiveScreen(
                         modifier = Modifier
                             .sharedElement(
                                 sharedContentState = rememberSharedContentState(key = "APOD${it.url}"),
-                                animatedVisibilityScope = animatedVisibilityScope)
+                                animatedVisibilityScope = animatedVisibilityScope
+                            )
                             .wrapContentHeight()
                             .padding(5.dp)
                             .clip(RoundedCornerShape(15.dp))
-                            .clickable {
+                            .border(
+                                1.5.dp,
+                                LocalContentColor.current.copy(0.25f),
+                                RoundedCornerShape(15.dp)
+                            )
+                            .clickable(onClick = {
                                 navController.navigate(
                                     HyleNavigation.APODArchiveScreen.APODDetailScreen(
                                         apod = Json.encodeToString(it)
                                     )
                                 )
-                            },
+                            }, interactionSource = remember {
+                                MutableInteractionSource()
+                            }, indication = null),
                         contentDescription = null,
                     )
                 }
