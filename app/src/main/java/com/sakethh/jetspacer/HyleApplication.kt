36d92cb5d94388e0d51aca4e7d198b13213ae0c0
sakethpathike <sakethpathike@gmail.com>
2025-08-18 15:09:48 +0530

    feat: set default UncaughtExceptionHandler to handle uncaught exceptions

--- app/src/main/java/com/sakethh/jetspacer/HyleApplication.kt
+++ app/src/main/java/com/sakethh/jetspacer/HyleApplication.kt
@@ -5,6 +5,8 @@ import com.sakethh.jetspacer.data.repository.PreferencesRepositoryImpl
 import com.sakethh.jetspacer.domain.useCase.PreferencesUseCase
 import com.sakethh.jetspacer.ui.screens.settings.SettingsScreenViewModel
 import com.sakethh.jetspacer.ui.screens.settings.SettingsScreenViewModel.Companion.dataStore
+import com.sakethh.jetspacer.ui.utils.UIChannel
+import com.sakethh.jetspacer.ui.utils.pushUIEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,6 +14,16 @@ import kotlinx.coroutines.launch
 class HyleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+
+            // usually this should redirect to a new activity and show the log, but this is fine for us
+            e.printStackTrace()
+            e.message?.let {
+                CoroutineScope(Dispatchers.Default).launch {
+                    pushUIEvent(type = UIChannel.Type.ShowSnackbar(it))
+                }
+            }
+        }
         CoroutineScope(Dispatchers.Default).launch {
             SettingsScreenViewModel(PreferencesUseCase(PreferencesRepositoryImpl(applicationContext.dataStore))).readAllSettingsValues()
         }
